a1bc11e26d7549baf11da8cc34f843de16b641f0
egapb <egapb@DESKTOP-GF4ORJ1>
2018-11-16 21:22:30 -0600

    Edgar

--- src/userInterface/EmployeeRegisterController.java
+++ src/userInterface/EmployeeRegisterController.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package userInterface;
 
 import java.net.URL;
@@ -16,10 +12,11 @@ import javafx.scene.control.Button;
  *
  * @author egapb
  */
-public class MainController implements Initializable {
+public class EmployeeRegisterController implements Initializable {
 
     
     @FXML private Button signUp_button;
+    @FXML private Button cancel_button;
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
