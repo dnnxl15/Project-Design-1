44b53b38b9e45e22164f4a6d78bbab1082d7d5a7
dnnxl15 <dnnxl15@hotmail.com>
2018-11-16 22:09:56 -0600

    Sign up interface

--- src/userInterface/MainController.java
+++ src/userInterface/MainController.java
@@ -7,14 +7,9 @@ package userInterface;
 
 import java.net.URL;
 import java.util.ResourceBundle;
-<<<<<<< HEAD
-
-=======
->>>>>>> branch 'master' of https://github.com/dnnxl15/Project-Design-1.git
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
-<<<<<<< HEAD
 import javafx.scene.control.TextField;
 
 /**
