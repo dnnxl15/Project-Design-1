004717c2345c2debc05603b894243b1c0fbf34fe
egapb <egapb@DESKTOP-GF4ORJ1>
2018-11-20 20:05:33 -0600

    Edgar

--- src/connection/ConnectionPool.java
+++ src/connection/ConnectionPool.java
@@ -5,10 +5,55 @@
  */
 package connection;
 
+import com.mysql.jdbc.Connection;
+import com.mysql.jdbc.*;
+import java.sql.DriverManager;
+
+import java.sql.*;
+import javax.swing.JOptionPane;
 /**
  *
  * @author egapb
  */
 public class ConnectionPool {
+    private static ConnectionPool connectionPoolD;
+    private static Connection conect;
+    private static final String driver = "org.gjt.mm.mysql.Driver";
+    private static final String user = "root";
+    private static final String url = "jdbc:mysql://localhost:3306/ovnidatabase";
+   
+    private ConnectionPool() {
+    }
     
-}
+    public static ConnectionPool getInstance(){
+              
+     if(connectionPoolD == null){
+         connectionPoolD = new ConnectionPool();
+        }
+     return connectionPoolD;
+    }
+    public Connection getConnection(){
+        if(conect == null){
+        try{
+          Class.forName(driver);
+          conect = (Connection) DriverManager.getConnection(url,user,"");
+             if(conect != null){
+              System.out.println("Conexcion");
+            }
+            }catch(Exception e){
+            JOptionPane.showMessageDialog(null, "Error de conexion de la base de datos");
+            }
+        }
+        
+        return conect;
+    }
+    
+    public void desconectar(){
+        conect = null;
+    }
+
+  
+    }
+    
+    
+
--- src/connection/ObjectPool.java
+++ src/connection/ObjectPool.java
@@ -1,7 +1,28 @@
 package connection;
 import com.mysql.jdbc.*;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 public class ObjectPool {
-    Long expirationTime;
-    Connection conect;
     
+    public static void main(String [] args)
+{
+    ConnectionPool con = ConnectionPool.getInstance();
+    Connection ing = con.getConnection();
+    String sql = "INSERT INTO person(personID,name,lastname,identification) VALUES (?,?,?,?)";
+        try {
+            PreparedStatement stm = (PreparedStatement) ing.prepareStatement(sql);
+            stm.setInt(1, 9);
+            stm.setString(2,"eparra");
+            stm.setString(3,"prueba");
+            stm.setInt(4, 123546);
+            stm.executeUpdate();
+        } catch (SQLException ex) {
+            Logger.getLogger(ObjectPool.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    con.desconectar();
+    
+}
+    
+
 }
