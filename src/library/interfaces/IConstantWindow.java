04523ac22aa9bdfc9ee7622bd54ce628f24d4e4e
dnnxl15 <dnnxl15@hotmail.com>
2018-11-22 11:37:05 -0600

    Constant Window added

--- src/library/interfaces/IConstantWindow.java
+++ src/library/interfaces/IConstantWindow.java
@@ -6,4 +6,5 @@ public interface IConstantWindow
 	public final String SIGN_UP_CLIENT_WINDOW_UI = "ClientRegister.fxml";
 	public final String SIGN_IN_CLIENT_WINDOW_UI = "ClientWindow.fxml";
 	public final String MANAGER_WINDOW = "ManagerWindow.fxml";
+	public final String EMPLOYEE_REGISTER_WINDOW = "EmployeeRegister.fxml";
 }
