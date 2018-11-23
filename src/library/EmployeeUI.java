99cd71232c43fb68153de7ed1e40b957a20a61d3
dnnxl15 <dnnxl15@hotmail.com>
2018-11-23 14:21:06 -0600

    New changes

--- src/library/EmployeeUI.java
+++ src/library/EmployeeUI.java
@@ -7,8 +7,44 @@ public class EmployeeUI
 	private String name;
 	private String lastname;
 	private String email;
-	private String salary;
+	private double salary;
 	private String rol;
 	private CheckBox check;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getLastname() {
+		return lastname;
+	}
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getRol() {
+		return rol;
+	}
+	public void setRol(String rol) {
+		this.rol = rol;
+	}
+	public CheckBox getCheck() {
+		return check;
+	}
+	public void setCheck(CheckBox check) {
+		this.check = check;
+	}
+	public double getSalary() {
+		return salary;
+	}
+	public void setSalary(double salary) {
+		this.salary = salary;
+	}
 
 }
--- src/userInterface/EmployeeViewController.java
+++ src/userInterface/EmployeeViewController.java
@@ -1,15 +1,65 @@
 package userInterface;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import controller.Restaurant;
+import javafx.beans.property.SimpleDoubleProperty;
+import javafx.beans.property.SimpleFloatProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.control.Alert.AlertType;
+import library.EmployeeUI;
 import library.RestaurantUI;
 
-public class EmployeeViewController 
+public class EmployeeViewController extends Controller implements Initializable
 {
-	@FXML private TableView<RestaurantUI> restaurant_table;
-	@FXML private TableColumn<RestaurantUI, String> columnAddress;
-	@FXML private TableColumn<RestaurantUI, String> columnLegalName;
+	@FXML private TableView<EmployeeUI> employee_table;
+	@FXML private TableColumn<EmployeeUI, String> columnName;
+	@FXML private TableColumn<EmployeeUI, String> columnLastaname;
+	@FXML private TableColumn<EmployeeUI, String> columnEmail;
+	@FXML private TableColumn<EmployeeUI, Number> columnSalary;
+	@FXML private TableColumn<EmployeeUI, String> columnRol;
+	@FXML private TableColumn<EmployeeUI, CheckBox> columnDisable;
+	@FXML private Button cancel_button;
+	@FXML private Label result_label;
+	
+	
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) 
+	{
+		columnName.setCellValueFactory(cellData->new SimpleStringProperty(cellData.getValue().getName()));
+		columnLastaname.setCellValueFactory(cellData->new SimpleStringProperty(cellData.getValue().getLastname()));
+		columnEmail.setCellValueFactory(cellData->new SimpleStringProperty(cellData.getValue().getEmail()));
+		columnSalary.setCellValueFactory(cellData->new SimpleDoubleProperty(cellData.getValue().getSalary()));
+		columnRol.setCellValueFactory(cellData->new SimpleStringProperty(cellData.getValue().getRol()));
+		columnDisable.setCellValueFactory(new PropertyValueFactory<EmployeeUI, CheckBox>("check"));
 
+	
+		ObservableList<EmployeeUI> newListEmployee = null;
+		//newListRestaurant = FXCollections.observableArrayList(Restaurant.getInstance().getRestaurant());//ControlSystem.getInstance().loadProduct());
+		//mount_restaurant_label.setText("("+newListRestaurant.size()+")");
 
+		employee_table.setItems(newListEmployee);
+		result_label.setText("("+newListEmployee.size()+")");
+	}
+	
+	public void closeWindow()
+	{
+		boolean value = showAlert(AlertType.CONFIRMATION, "Close window", "Are you sure to close the window?");
+		if(value)
+		{
+			closeWindow(cancel_button);
+		}
+	}
 }
