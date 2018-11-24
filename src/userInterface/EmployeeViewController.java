da340719df14f087d3f40287e8844ac856304b63
dnnxl15 <dnnxl15@hotmail.com>
2018-11-24 08:24:38 -0600

    Employee view changes

--- src/userInterface/EmployeeViewController.java
+++ src/userInterface/EmployeeViewController.java
@@ -1,5 +1,6 @@
 package userInterface;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -14,15 +15,18 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.control.Alert.AlertType;
 import library.EmployeeUI;
 import library.RestaurantUI;
+import library.interfaces.IConstantWindow;
 
-public class EmployeeViewController extends Controller implements Initializable
+public class EmployeeViewController extends Controller implements Initializable, IConstantWindow
 {
 	@FXML private TableView<EmployeeUI> employee_table;
 	@FXML private TableColumn<EmployeeUI, String> columnName;
@@ -33,7 +37,10 @@ public class EmployeeViewController extends Controller implements Initializable
 	@FXML private TableColumn<EmployeeUI, CheckBox> columnDisable;
 	@FXML private Button cancel_button;
 	@FXML private Label result_label;
-	
+	@FXML private TextField name_textfield;
+	@FXML private TextField lastname_textfield;
+	@FXML private TextField slary_textfield;
+	@FXML private ComboBox<String> role_combobox;
 	
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) 
@@ -60,6 +67,46 @@ public class EmployeeViewController extends Controller implements Initializable
 		if(value)
 		{
 			closeWindow(cancel_button);
+		}
+	}
+	
+	public void updateEmployee()
+	{
+		String lastname = lastname_textfield.getText().toString();
+    	int salary;
+    	String name = name_textfield.getText().toString();
+    	String value = role_combobox.getSelectionModel().getSelectedItem();
+    	try
+		{
+    		salary =  Integer.parseInt(slary_textfield.getText().toString());
+		}
+		catch(Exception e)
+		{
+			showAlert(AlertType.ERROR ,"Salary" , "Must be a number");
+			return;
+		}
+    	if(name.equals(EMPTY) || lastname.equals(EMPTY))
+    	{
+			showAlert(AlertType.ERROR ,"All fields" , "All fields must be complete");
+			return;
+    	}
+    	else
+    	{
+   			//Restaurant.getInstance().updateCombo(product.getIdPerson(), name, price, description, value);//////////////////////////////////////////////////////
+   			showAlert(AlertType.CONFIRMATION ,"Combo update" , "Combo is updated in the system");
+   			lastname_textfield.setText(EMPTY);
+   			name_textfield.setText(EMPTY);
+   			slary_textfield.setText(EMPTY);
+   			initialize(null, null);
+    	}
+	}
+	
+	public void openSalaryWindow()
+	{
+		try {
+			openWindow(EMPLOYEE_SALARY_VIEW_WINDOW, MAX_HEIGHT_WINDOW, MAX_WIDTH_WINDOW, OVNI_IMAGE_COLOR_PATH, OVNIRESTAURANT_TITLE);
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
 	}
 }
